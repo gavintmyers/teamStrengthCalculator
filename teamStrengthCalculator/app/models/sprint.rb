0df2acbd8a2176dd4a331a389b23ae5b6ef50d1a
Gavin Myers <gavin.myers@annkissam.com>
2016-03-08 14:43:58 -0500

    Update sprint more.

--- teamStrengthCalculator/app/models/sprint.rb
+++ teamStrengthCalculator/app/models/sprint.rb
@@ -1,7 +1,14 @@
 class Sprint < ActiveRecord::Base
+
 	validate :start_before_end
 	validates :iteration_length, presence: true
 	validates :part_time_multiplier, presence: true
+
+	has_many :hours
+	has_many :clients through :hours
+	has_many :developers through :hours
+
+	accepts_nested_attributes_for :hours
 
 	def start_before_end
 		if self.start_date >= self.end_date
