5c90f74695bbf87d4b8e2e867fcad1f67e56ea52
Gavin Myers <gavin.myers@annkissam.com>
2016-03-08 12:30:03 -0500

    Add validate to number of hours entry

--- teamStrengthCalculator/db/schema.rb
+++ teamStrengthCalculator/db/schema.rb
@@ -11,23 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20160307212449) do
+ActiveRecord::Schema.define(version: 20160308153220) do
 
   create_table "clients", force: :cascade do |t|
     t.text     "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-=======
-ActiveRecord::Schema.define(version: 20160307213425) do
->>>>>>> a50bb1d139c8a78d58c4c2668202f57b2b8cb5b7
 
   create_table "developers", force: :cascade do |t|
     t.text     "name"
     t.text     "dev_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "hours", force: :cascade do |t|
+    t.integer  "client"
+    t.integer  "developer"
+    t.integer  "sprint"
+    t.integer  "number_of_hours"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "sprints", force: :cascade do |t|
