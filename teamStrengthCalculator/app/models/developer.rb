9a531678dc812c154e9b0ad41c88397d6aae4499
Gavin Myers <gavin.myers@annkissam.com>
2016-03-17 15:13:57 -0400

    Add has_many (through Hours) between Clients and Developers.

--- teamStrengthCalculator/app/models/developer.rb
+++ teamStrengthCalculator/app/models/developer.rb
@@ -4,5 +4,6 @@ class Developer < ActiveRecord::Base
     message: "must be either 'Part Time', 'Full Time', or 'Special Guest'"}
 
   has_many :hours
+  has_many :clients, through: :hours
   has_and_belongs_to_many :sprints
 end
